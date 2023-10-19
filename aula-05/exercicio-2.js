0f386aadfca294a16f63a96056bb4f988a18ba40
Ivirson <ivi_daltro@hotmail.com>
2023-10-18 21:59:59 -0300

    Aula 05 - Exercicios

--- aula-05/exercicio-2.js
+++ aula-05/exercicio-2.js
@@ -1,8 +1,3 @@
 /*
-    Crie uma função "validarSenha" que recebe uma senha como parâmetro e retorna true se a senha atender aos seguintes critérios, sem utilizar regex:
-
-    Pelo menos 8 caracteres de comprimento.
-    Deve conter pelo menos uma letra maiúscula.
-    Deve conter pelo menos uma letra minúscula.
-    Deve conter pelo menos um número.
+    Crie uma função "gerarSenhaAleatoria" que recebe um comprimento (tamanho) como parâmetro e gera uma senha aleatória contendo letras maiúsculas, letras minúsculas e números.
 */
--- aula-05/exercicio-3.js
+++ aula-05/exercicio-3.js
@@ -1,3 +1,8 @@
 /*
-    Crie uma função "gerarSenhaAleatoria" que recebe um comprimento como parâmetro e gera uma senha aleatória contendo letras maiúsculas, letras minúsculas, números e caracteres especiais.
+    Crie uma função "validarSenha" que recebe uma senha como parâmetro e retorna true se a senha atender aos seguintes critérios, sem utilizar regex:
+
+    Pelo menos 8 caracteres de comprimento.
+    Deve conter pelo menos uma letra maiúscula.
+    Deve conter pelo menos uma letra minúscula.
+    Deve conter pelo menos um número.
 */
